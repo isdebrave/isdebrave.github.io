83a2b3036ba5ab711196b845e4b86a812ddfb592
Kim Seok Yoon <isdebrave@gmail.com>
2024-06-16 17:29:26 +0900

    PostList 정리

--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -1,15 +1,7 @@
 import styled from "@emotion/styled";
 import React from "react";
 import PostItem, { PostItemType } from "./PostItem";
-
-type PostListType = {
-  posts: {
-    node: {
-      id: string;
-      frontmatter: PostItemType;
-    };
-  }[];
-};
+import { graphql, useStaticQuery } from "gatsby";
 
 const Grid = styled.div`
   display: grid;
@@ -19,8 +11,38 @@ const Grid = styled.div`
   margin: 0 auto;
 `;
 
-const PostList: React.FC<PostListType> = (props) => {
-  const { posts } = props;
+const PostList = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+      ) {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              summary
+              date(formatString: "YYYY.MM.DD")
+              categories
+              thumbnail {
+                childImageSharp {
+                  gatsbyImageData(width: 768, height: 400)
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const posts = data.allMarkdownRemark.edges as Array<{
+    node: {
+      id: string;
+      frontmatter: PostItemType;
+    };
+  }>;
 
   return (
     <Grid>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import styled from "@emotion/styled";
 import Layout from "components/layout/Layout";
-import { PostItemType } from "components/post/PostItem";
 import PostList from "components/post/PostList";
 import { graphql } from "gatsby";
 import React from "react";
@@ -13,14 +12,6 @@ type IndexPageType = {
         title: string;
       };
     };
-    allMarkdownRemark: {
-      edges: {
-        node: {
-          id: string;
-          frontmatter: PostItemType;
-        };
-      }[];
-    };
   };
 };
 
@@ -30,11 +21,15 @@ const ContentWrapper = styled.main`
   padding: 70px 20px;
   flex: 1;
   background-color: #eeebe0;
+
+  & > h1 {
+    text-align: center;
+    margin-bottom: 40px;
+  }
 `;
 
 const IndexPage: React.FC<IndexPageType> = (props) => {
   const { title } = props.data.site.siteMetadata;
-  const { edges } = props.data.allMarkdownRemark;
 
   return (
     <>
@@ -42,8 +37,8 @@ const IndexPage: React.FC<IndexPageType> = (props) => {
       <div style={{ display: "flex", minHeight: "100vh" }}>
         <Layout />
         <ContentWrapper>
-          <h1 style={{ textAlign: "center", marginBottom: "40px" }}>{title}</h1>
-          <PostList posts={edges} />
+          <h1>{title}</h1>
+          <PostList />
         </ContentWrapper>
       </div>
     </>
@@ -57,26 +52,6 @@ export const getPostList = graphql`
     site {
       siteMetadata {
         title
-      }
-    }
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
-    ) {
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            summary
-            date(formatString: "YYYY.MM.DD")
-            categories
-            thumbnail {
-              childImageSharp {
-                gatsbyImageData(width: 768, height: 400)
-              }
-            }
-          }
-        }
       }
     }
   }
